c89757e994d9da5a6e2ec280cda789603c4454e0
ropinjo <ropinjo@gmail.com>
2026-05-31 23:23:00 +0200

    Bump version to 1.2 and update versionCode to 3 for the next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = currentVersionCode
-        versionName = "1.0"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
